e519df75235b8676251ba8417498b9d809f995a8
Bharathkumar1522 <bk152002@gmail.com>
2026-04-24 10:59:24 +0530

    fix: revert dynamic imports to static imports to resolve NO_FCP error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { HeroSection } from "@/components/sections/Hero";
-import dynamic from "next/dynamic";
-
-const IndustriesSection = dynamic(() => import("@/components/sections/Industries").then(mod => mod.IndustriesSection));
-const ComparisonSection = dynamic(() => import("@/components/sections/Comparison").then(mod => mod.ComparisonSection));
-const GlobalSection = dynamic(() => import("@/components/sections/GlobalReady").then(mod => mod.GlobalSection));
-const SecuritySection = dynamic(() => import("@/components/sections/Security").then(mod => mod.SecuritySection));
-const ContactSection = dynamic(() => import("@/components/sections/Contact").then(mod => mod.ContactSection));
+import { IndustriesSection } from "@/components/sections/Industries";
+import { ComparisonSection } from "@/components/sections/Comparison";
+import { GlobalSection } from "@/components/sections/GlobalReady";
+import { SecuritySection } from "@/components/sections/Security";
+import { ContactSection } from "@/components/sections/Contact";
 
 export default function Home() {
   return (
